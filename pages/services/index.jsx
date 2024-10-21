1fa5fac59da16f64f3f3fee2c4aff5d0c0f6b25f
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-10-21 14:18:39 +0300

    services  page

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -1,5 +1,54 @@
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
+import Circles from '../../components/Circles'
+import Bulb from '../../components/Bulb'
+import ServiceSlider from '../../components/ServiceSlider'
+
 const Services = () => {
-	return <div>Services</div>
+	return (
+		<div className='h-dvh bg-primary/30 py-36 flex justify-center m-auto  items-center '>
+			<Circles />
+			<div className='container mx-auto '>
+				<div className='flex flex-col  xl:flex-row gap-x-8 '>
+					{/* text */}
+					<div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+						<motion.h2
+							variants={fadeIn('up', 0.3)}
+							initial='hidden'
+							animate='show'
+							exit='hidden'
+							className='h2 xl:mt-8 capitalize'
+						>
+							my services <span className='text-accent'>.</span>
+						</motion.h2>
+						<motion.p
+							variants={fadeIn('up', 0.6)}
+							initial='hidden'
+							animate='show'
+							exit='hidden'
+							className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+						>
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
+							repellendus numquam cum, a dolorum itaque qui ipsa quas nesciunt
+							animi facilis quaerat odio veniam dolor asperiores omnis commodi,
+							quia cumque.
+						</motion.p>
+					</div>
+					<motion.div
+						variants={fadeIn('down', 0.9)}
+						initial='hidden'
+						animate='show'
+						exit='hidden'
+						className='w-full xl:max-w-[65%]'
+					>
+						{/* slider */}
+						<ServiceSlider />
+					</motion.div>
+				</div>
+			</div>
+			<Bulb />
+		</div>
+	)
 }
 
 export default Services
