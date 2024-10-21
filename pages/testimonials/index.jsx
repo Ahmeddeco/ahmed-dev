78c42c707c37d7a42f3281cb296613da14adda59
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-10-21 16:20:14 +0300

    Testimonials page

--- pages/testimonials/index.jsx
+++ pages/testimonials/index.jsx
@@ -1,5 +1,34 @@
+import TestimonialSlider from '../../components/TestimonialSlider'
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
+
 const Testimonials = () => {
-	return <div>Testimonials</div>
+	return (
+		<div className='h-dvh bg-primary/30 py-32 text-center '>
+			<div className='container mx-auto h-full flex flex-col justify-center'>
+				{/* title */}
+				<motion.h2
+					variants={fadeIn('up', 0.3)}
+					initial='hidden'
+					animate='show'
+					exit='hidden'
+					className='h2 capitalize mb-8 xl:mb-0'
+				>
+					what clients <span className='text-accent'>say.</span>
+				</motion.h2>
+				{/* slider */}
+				<motion.div
+					variants={fadeIn('up', 0.6)}
+					initial='hidden'
+					animate='show'
+					exit='hidden'
+					className=''
+				>
+					<TestimonialSlider />
+				</motion.div>
+			</div>
+		</div>
+	)
 }
 
 export default Testimonials
